fa9c3a61d1db0d3360b07eb41f7d911c7f82a4d3
quentinMlvl <quentin.malavielle@gmail.com>
2021-03-03 15:56:42 +0100

    TD3 Création de la liste des personnages

--- lib/widgets/CharacterMaster.dart
+++ lib/widgets/CharacterMaster.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:the_battle/models/Character.dart';
+import 'package:the_battle/widgets/CharacterPreview.dart';
 import 'package:the_battle/data/characters.dart';
 
 class CharacterMaster extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              for(var i in characters) Text(i.name)
-            ]),
+      body: ListView.builder(
+        itemCount: characters.length,
+        itemBuilder: (BuildContext context, int index) {
+          final Character character = characters[index];
+
+          return CharacterPreview(character: character);
+        },
       ),
     );
   }
--- lib/widgets/CharacterPreview.dart
+++ lib/widgets/CharacterPreview.dart
@@ -1,3 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:the_battle/models/Character.dart';
-import 'package:the_battle/data/characters.dart';
+
+class CharacterPreview extends StatelessWidget {
+  const CharacterPreview({Key key, this.character}) : super(key: key);
+
+  final Character character;
+
+  Color _getColor() {
+    // if (this.character.autoSelected) {
+    //   return Colors.green[800];
+    // } else if (this.character.selected &&
+    //     this.character.autoSelected == false) {
+    //   return Colors.green[600];
+    // } else {
+    return (false) ? Colors.red[300] : Colors.red[400];
+    // }
+  }
+
+  // bool _getEnabled(){
+  //   return !(this.character.autoSelected ||
+  //     this.character.selected);
+  // }
+
+  // void _onTap() {
+  //   this.onSelected(this.character):
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: _getColor(),
+      child: ListTile(
+        leading: Image(
+          image: AssetImage(character.imagePath()),
+          width: 200,
+        ),
+        title: Text(
+          character.name,
+          style: TextStyle(
+              fontFamily: 'Knewave', fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
